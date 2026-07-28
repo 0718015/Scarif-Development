c71083703a56a11c348f85ab981befbea33b752f
0718015 <0718015@schoolsnet.act.edu.au>
2026-07-28 14:48:31 +1000

    Updated Code

--- iot/src/main.cpp
+++ iot/src/main.cpp
@@ -1,19 +1,60 @@
-#include <Arduino.h> 
 
-void setup() 
+// Set a unique identifier for your device before importing comms.h
+const char *mqttClient = "ESP32_Matt"; // EDIT THIS FIELD
+
+const char *mqttTopic;
+
+#include <Arduino.h>
+#include "comms.h"
+
+void performActionBasedOnPayload(String payload)
 {
-    pinMode(13, OUTPUT);
+    Serial.print("Payload received: ");
+    Serial.println(payload);
+
+    // Turn built-in LED ON if payload starts with '1', else OFF
+    if (payload.length() > 0 && payload[0] == '1')
+    {
+        Serial.println("Action: LED ON");
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else
+    {
+        Serial.println("Action: LED OFF");
+        digitalWrite(LED_BUILTIN, LOW);
+    }
 }
 
-
-void loop() 
+void setup()
 {
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+    
+    wifiSetup();
+    mqttSetup();
 
-    digitalWrite(13, HIGH);
+    while (!Serial)
+    {
+        delay(10);
+    }
     delay(1000);
-    digitalWrite(13, LOW);
-    delay(1000);
+}
 
+void loop()
+{
+    // 1. Maintain connection to the broker
+    mqttConnect();
+
+    // 2. Transmit periodic telemetry (if required by design specification)
+    unsigned long now = millis();
+    if (now - lastUpdate > updateInterval)
+    {
+        lastUpdate = now;
+        // TODO: Insert customized sendDataToServer() calls here.
+    }
+
+    // 3. Yield execution time for PubSubClient processing
+    client.loop();
     delay(100);
 }
 
